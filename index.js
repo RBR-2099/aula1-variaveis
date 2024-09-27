093ca7f558e56ca4a612ad6be93b2fc8abb8907a
Rodrigo Reis <rodrigo.br.reis@gmail.com>
2024-09-26 21:46:11 -0300

    comitando para o servidor

---
+++ index.js
@@ -0,0 +1,16 @@
+//Isso é um comentário em apenas uma linha
+/*Isso é um
+comentário
+em várias
+linhas*/
+
+//alert ("Olha o JS aqui")
+
+let nome = prompt('Qual é o seu nome?')
+
+alert ('Seja bem vindo ' + nome + '!')
+//alert ('Seja bem vindo ${nome}!')
+
+
+
+console.log ('imprime apenas no console no navegador')
