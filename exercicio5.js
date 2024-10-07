8a40e733192185a043ee3e2e078656d8224ed5d9
Rodrigo Reis <rodrigo.br.reis@gmail.com>
2024-10-07 19:10:08 -0300

    adicionando exercícios que faltavam (5 e 5)

---
+++ exercicio5.js
@@ -0,0 +1,17 @@
+//Escreva um programa que faça 3 perguntas de Sim ou Não, armazenado em uma variável. Por exemplo: "Você está usando uma roupa azul hoje?". Depois, siga os passos:
+//a) Crie três novas variáveis, contendo as respostas
+//b) Imprima na tela todas as perguntas seguidas por suas respectivas respostas. Por exemplo:
+//Você está usando uma roupa azul hoje? - SIM
+
+let pergunta1
+let pergunta2
+let pergunta3
+
+pergunta1 = prompt ('Você é estudante?')
+pergunta2 = prompt ('Você estuda no SENAC?')
+pergunta3 = prompt ('Você está estudando Lógica de Programação?')
+
+// console.log ('Você é estudante?', pergunta1 , 'Você estuda no SENAC?' , pergunta2, 'Você está estudando Lógica de Programação?', pergunta3)
+
+
+console.log ('Você é estudante? - ' , pergunta1 , ' | Você estuda no SENAC? - ' , pergunta2, ' | Você está estudando Lógica de Programação? - ', pergunta3)
